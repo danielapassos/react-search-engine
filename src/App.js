95305a392069e0674a9ea5258acc882be9111029
Daniela Passos <moitidani@gmail.com>
2022-09-10 12:01:17 +0300

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <div className="container">
+      <h1>Weather Search App</h1>
+      <footer>
+        This project was created by <a href="https://danielapassos.com" target="_blank">Daniela Passos</a> and is {" "}
+      <a href="https://github.com/danielapassos/react-search-engine" target="_blank">open-sourced on Github.</a>
+      </footer>
+      </div>
     </div>
   );
 }
