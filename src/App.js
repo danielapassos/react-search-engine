624f42f350f67ac42ce1f590f8318797e1009859
Daniela Passos <moitidani@gmail.com>
2022-09-10 17:26:35 +0300

    Updated and added all components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-
-import './App.css';
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>Weather Search App</h1>
-      <footer>
-        This project was created by <a href="https://danielapassos.com" target="_blank">Daniela Passos</a> and is {" "}
-      <a href="https://github.com/danielapassos/react-search-engine" target="_blank">open-sourced on Github.</a>
-      </footer>
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://danielapassos.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Daniela Passos
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/danielapassos/react-search-engine"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://marvelous-speculoos-78e033.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
       </div>
     </div>
   );
-}
+  }
